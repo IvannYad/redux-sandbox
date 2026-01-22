8189ad272c69e2c061f51d58bd219adfa4cccbf4
Ivan Yadchyshyn <113620149+IvannYad@users.noreply.github.com>
2026-01-22 19:45:55 +0200

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+  },
+});
+
+const startingState = store.getState();
+console.log(startingState);
+
+store.dispatch(songsSlice.actions.addSong("New Song"));
+
+const finalState = store.getState();
+console.log(finalState);
+
+export { store };
+export const { addSong, removeSong } = songsSlice.actions;
