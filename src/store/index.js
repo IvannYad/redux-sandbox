ed5ff3ccdbf98ccc4e52d15dc6c8219e39b8fe82
Ivan Yadchyshyn <113620149+IvannYad@users.noreply.github.com>
2026-01-23 17:57:45 +0000

    completed project

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,21 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-const songsSlice = createSlice({
-  name: "song",
-  initialState: [],
-  reducers: {
-    addSong(state, action) {
-      state.push(action.payload);
-    },
-    removeSong(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { reset } from "./actions";
 
 const store = configureStore({
   reducer: {
-    songs: songsSlice.reducer,
+    songs: songsReducer,
+    movies: moviesReducer,
   },
 });
 
 const startingState = store.getState();
 console.log(startingState);
 
-store.dispatch(songsSlice.actions.addSong("New Song"));
+// store.dispatch(songsSlice.actions.addSong("New Song"));
 
 const finalState = store.getState();
 console.log(finalState);
 
-export { store };
-export const { addSong, removeSong } = songsSlice.actions;
+export { store, reset, addSong, removeSong, addMovie, removeMovie };
